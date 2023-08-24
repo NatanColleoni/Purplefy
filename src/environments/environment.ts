49abf2b80df2ccf56c6a36ad6ff6092cc948a22c
NatanColleoni <62671225+NatanColleoni@users.noreply.github.com>
2023-08-24 10:51:22 -0300

    criação de arquivos environment, variaveis, e stylePreprocessorOptions

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,3 +2,18 @@ export const environment = {
   production: false
 }
 
+export const SpotifyConfig = {
+  clientId: '6a40c0e71f984d45b1c834859a12f3ef',
+  authEndpoint: 'https://accounts.spotify.com/authorize',
+  redirectUrl: 'http://localhost:4200/login/',
+  scopes: [
+    "user-read-currently-playing", //tocando agora
+    "user-read-recently-played", //tocada recentemente
+    "user-read-playback-state", //estado do player
+    "user-top-read", //mais escutados
+    "user-modify-playback-state", //mudar o estado do player
+    "user-library-read", //visualizar biblioteca
+    "playlist-read-private", //visualizar playlist privada
+    "playlist-read-collaborative" //visualizar playlist colaborativa
+  ]
+}
