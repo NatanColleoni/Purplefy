b88697f0b801daa0e0c2413008d497ff071772ea
Natan Fonseca <TR039271@MinhaTI.com.br>
2023-08-25 16:28:02 -0300

    Criação componente de login

---
+++ src/app/pages/login/login.module.ts
@@ -0,0 +1,12 @@
+import { LoginComponent } from './login.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [],
+  imports: [
+    LoginComponent,
+    CommonModule
+  ],
+})
+export class LoginModule { }
