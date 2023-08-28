70bf1cbf8df3e7a19a3fe38c891ecb3c9fe2339c
NatanColleoni <62671225+NatanColleoni@users.noreply.github.com>
2023-08-28 15:47:20 -0300

    Estilização inicial pagina de login e autenticação integrada com spotify

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SpotifyService } from 'src/app/services/spotify.service';
 
 @Component({
   selector: 'app-login',
@@ -7,10 +8,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   
-  constructor(){}
+  constructor(
+    private spotifyService: SpotifyService
+  ) {
+
+  }
 
   ngOnInit(): void {
     
   }
   
+  openLoginPage() {
+    window.location.href = this.spotifyService.getLoginUrl()
+  }
 }
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -1,12 +1,17 @@
-import { LoginComponent } from './login.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+import { LoginComponent } from './login.component';
+import { LoginRoutes } from './login.routes';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    LoginComponent
+  ],
   imports: [
-    LoginComponent,
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(LoginRoutes)
   ],
 })
 export class LoginModule { }
