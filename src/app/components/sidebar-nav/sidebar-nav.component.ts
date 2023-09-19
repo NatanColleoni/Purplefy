b1e0dc846145c35a38c004026205d31d8f0e5c21
NatanColleoni <62671225+NatanColleoni@users.noreply.github.com>
2023-09-19 20:21:56 -0300

    Criação de componente botão e botões parte superior

--- src/app/components/sidebar-nav/sidebar-nav.component.ts
+++ src/app/components/sidebar-nav/sidebar-nav.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { faHome, faSearch, faGuitar, faMusic } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-sidebar-nav',
   templateUrl: './sidebar-nav.component.html',
   styleUrls: ['./sidebar-nav.component.scss']
 })
-export class SidebarNavComponent {
+export class SidebarNavComponent implements OnInit {
 
+  homeIcon = faHome
+  searchIcon = faSearch
+  artistIcon = faGuitar
+  playlistIcon = faMusic
+
+  constructor() { }
+
+  ngOnInit(): void {
+    
+  }
 }
--- src/app/pages/player/player.module.ts
+++ src/app/pages/player/player.module.ts
@@ -4,14 +4,18 @@ import { PlayerComponent } from './player.component';
 import { RouterModule } from '@angular/router';
 import { PlayerRoutes } from './player.routes';
 import { SidebarNavComponent } from 'src/app/components/sidebar-nav/sidebar-nav.component';
+import { MenuButtonComponent } from 'src/app/components/menu-button/menu-button.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
     PlayerComponent,
-    SidebarNavComponent
+    SidebarNavComponent,
+    MenuButtonComponent
   ],
   imports: [
     CommonModule,
+    FontAwesomeModule,
     RouterModule.forChild(PlayerRoutes)
   ]
 })
